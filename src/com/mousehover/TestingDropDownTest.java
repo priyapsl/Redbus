aa73ff2b7a9e6cff1d7853a9d558c043441b519c
priya vyawahare <priya_vyawahare@persistent.co.in>
2018-05-17 17:50:30 +0530

    The second commit on redbus

--- src/com/mousehover/TestingDropDownTest.java
+++ src/com/mousehover/TestingDropDownTest.java
@@ -12,6 +12,7 @@ import junit.framework.Assert;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Reporter;
@@ -25,9 +26,17 @@ public class TestingDropDownTest {
 	public static WebDriver driver =  null;
 	public static Actions action = null;
 	public static Map<String , String[]> menumap = new HashMap<String, String[]>();
-	
 	public String [] menu1 = {"Selenium IDE","Selenium RC","Selenium Grid","Selenium Server"};
 	public String [] menu2 = {"Item 1","Item 2","Item 3","Item 4"};
+	
+	
+	@BeforeSuite
+	 public void menuDataprovider() {
+		 menumap.put("Mastering Selenium", menu1);
+		 menumap.put("Menu 2", menu2);
+		 menumap.put("Menu 3", menu2);
+		 menumap.put("Menu 4", menu2);
+		 }
 	
 	@BeforeTest
 	 public void openBroser() {
@@ -37,7 +46,6 @@ public class TestingDropDownTest {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		}
-								
 	
 	  @Test
 	  public void menuItem()
@@ -70,38 +78,46 @@ public class TestingDropDownTest {
 	  }
 
 	 
-		  
+		//Abstract method to perform mouse over and click on Main menu  
 	  public void clickonMenu1(String fmainmenu, String fsubmenu ){
 	    
 		 action = new Actions(driver);
-		 action.moveToElement(driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td/p[text()= '"+fmainmenu+"']"))).build().perform();
-		 driver.findElement(By.linkText(fsubmenu)).click();
-		 System.out.println(driver.getTitle());
-		 waitforpage(100);
-	     driver.navigate().back();
-       
-	 }
-
-	 
-	  public void waitforpage(long time){
+		 WebElement main_menu_element = driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td/p[text()= '"+fmainmenu+"']"));
+		 try {
+			     if(main_menu_element.isDisplayed()){
+			         
+			    	 WebElement sub_menu_element = driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td/p[text()= '"+fmainmenu+"']"));
+					  if(sub_menu_element.isDisplayed()){
+			        
+						 action.moveToElement(driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td/p[text()= '"+fmainmenu+"']"))).build().perform();
+						 driver.findElement(By.linkText(fsubmenu)).click();
+						 System.out.println(driver.getTitle());
+						 waitforpage(100);
+					     driver.navigate().back();
+					     }
+					   else{
+						   Reporter.log("The submenu " + main_menu_element+ " is not present");
+					       }
+					}
+			     else{
+			    	 Reporter.log("The Mainmenu " + main_menu_element+ "is not present");
+			     }
+			         
+			 }
+			 catch(Exception e)
+			 {
+				e.printStackTrace();
+			 }
+	      }
+      
+	  //Abstract method to wait on page
+	 public void waitforpage(long time){
 		 try {
 				Thread.sleep(time);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	   }
-    
-	 
-	 @BeforeSuite
-	 public void menuDataprovider() {
-		 menumap.put("Mastering Selenium", menu1);
-		 menumap.put("Menu 2", menu2);
-		 menumap.put("Menu 3", menu2);
-		 menumap.put("Menu 4", menu2);
-		 
-		 
-	 }
 	 
 	 @AfterTest
 	  public void TearDown() {
